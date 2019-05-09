47be04981cadbf6c4ead10e8ba8b3b4c8f3d786b
maxwellbenton <maxwellbenton@gmail.com>
2019-05-09 17:42:52 -0400

    adds solution

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,6 @@
-# Add code from Readme
+class BirdsController < ApplicationController
+    def index
+      @birds = Bird.all
+      render 'birds/index.html.erb'
+      end
+  end
--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,1 +1,2 @@
-# Add code from Readme
+class Bird < ApplicationRecord
+end
